774a7634bf370d7d06f565576f5e23440f93f33b
Alexandre Jezus <alexandrejezus.p@gmail.com>
2024-06-11 10:22:27 -0300

    commit 11/06

---
+++ adivinhacao.js
@@ -0,0 +1,12 @@
+let usuario;
+const aleatorio = Math.floor(Math.random() * 300 + 1);
+
+console.log("Adivinhe um número");
+process.stdin.on("data", function (data) {
+  usuario = Number(data.toString().trim());
+  if (usuario == aleatorio) {
+    console.log("O seu número está correto");
+  } else if (usuario != aleatorio) {
+    console.log("O seu número não está correto, tente outra vez.");
+  }
+});
